7d8564de304a8ad2471a8078ee8b1f29e609b95f
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-11-20 20:53:28 +0300

    feat(dev): 🚀 devcontainer-rust added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,13 @@
+set -euxo pipefail
+
+sudo apt-get update
+sudo apt-get install -y python3-dev python3-pip python3-venv libclang-dev bat exa 
+sudo python3 -m pip install cffi virtualenv pipx
+
+pipx ensurepath
+pipx install uniffi-bindgen
+pipx install cargo-deny
+pipx install pre-commit
+
+rustup update
+curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
