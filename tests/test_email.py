bd4803c7c1d78afda02b01e0cc5b0b9a046ed1d8
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-11-24 05:35:51 +0300

    feat(email-tests): 🧪 more email tests are added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- tests/test_email.py
+++ tests/test_email.py
@@ -5,7 +5,16 @@ import pytest
 # pytest parametrize
 @pytest.mark.parametrize(
     "email, expected",
-    [("email", False), ("example@example.com", True), ("example@example", False)],
+    [
+        ("email", False),
+        ("example@example.com", True),
+        ("example@example", False),
+        ("email@here.com", True),
+        ("weirder-email@here.and.there.com", True),
+        ("email@127.local.home.arpa", True),
+        ("example@valid-----hyphens.com", True),
+        ("example@valid-with-hyphens.com", True),
+    ],
 )
 def test_email(email: str, expected: bool):
     assert validx.email(email) == expected
