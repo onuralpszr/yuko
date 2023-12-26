ded34899aa6681a2ae6060580e12afc056b9376b
Onuralp SEZER <thunderbirdtr@gmail.com>
2023-12-27 02:13:10 +0300

    feat: 🚀 yuko __version__ added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- python/yuko/__init__.py
+++ python/yuko/__init__.py
@@ -1,5 +1,13 @@
 from yuko import yuko  # type: ignore[attr-defined]
 from yuko.yuko import *  # type: ignore[attr-defined] # noqa: F401, F403
+import importlib.metadata as importlib_metadata
+
+try:
+    # This will read version from pyproject.toml
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "development"
+
 
 __doc__ = yuko.__doc__
 if hasattr(yuko, "__all__"):
