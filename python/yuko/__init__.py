fe85adef950fe914f44d7e7a1f84e2ad46d07ba5
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-05-10 03:01:53 +0300

    refactor: remove outdated comment regarding version retrieval from pyproject.toml

--- python/yuko/__init__.py
+++ python/yuko/__init__.py
@@ -3,7 +3,6 @@ from yuko.yuko import *  # type: ignore[attr-defined] # noqa: F401, F403
 import importlib.metadata as importlib_metadata
 
 try:
-    # This will read version from pyproject.toml
     __version__ = importlib_metadata.version(__package__ or __name__)
 except importlib_metadata.PackageNotFoundError:
     __version__ = "development"
